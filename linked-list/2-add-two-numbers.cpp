f0ee60407aa26a8ea4cbdbefd50f0a796998aee6
GengYunTien <willytien88@gmail.com>
2026-09-10 06:50:29 +0800

    [Linkedlist] Add #2 Add Two Numbers

---
+++ linked-list/2-add-two-numbers.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// Solution: simulate addition with carry using dummy node
+// - traverse both lists simultaneously
+// - handle different lengths and remaining carry
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode dummy(0);
+    ListNode* current = &dummy;
+    int carry = 0;
+
+    while (l1 != nullptr || l2 != nullptr || carry != 0) {
+        int sum = carry;
+        if (l1 != nullptr) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        if (l2 != nullptr) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+        carry = sum / 10;
+        current->next = new ListNode(sum % 10);
+        current = current->next;
+    }
+    return dummy.next;
+}
